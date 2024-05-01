94b8c8c5e2c748d688dcc8cdf33dca0ea9c5e29a
RLee38 <133907554+RLee38@users.noreply.github.com>
2024-05-01 18:39:13 +0000

    Plan update, html and css upgrade

---
+++ script.js
@@ -0,0 +1,4 @@
+ArrayList<String> foodList = new ArrayList<String>();
+//Add all cheap foods which ingredients can easily be found in a supermarket to the list
+ArrayList<String> ingredList = new ArrayList<String>();
+//Add all ingredients in the dishes to the list
