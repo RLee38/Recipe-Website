efc5e9f8f61840489b09e7982753370ae0d5ffdb
RLee38 <133907554+RLee38@users.noreply.github.com>
2024-05-04 00:51:56 +0000

    BROKEN - some good code, glow stuff, lil JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-ArrayList<String> foodList = new ArrayList<String>();
-//Add all cheap foods which ingredients can easily be found in a supermarket to the list
-ArrayList<String> ingredList = new ArrayList<String>();
-//Add all ingredients in the dishes to the list
+function addFood() {
+  const ingredients = [];
+  ingredients[ingredients.length] = document.getElementById("IngredientBar").value;
+ console.log(ingredients[ingredients.length-1]);
 
+}
